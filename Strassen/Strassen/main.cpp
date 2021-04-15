7e7d36cfe79464c6ee1c5bdd1e57cc724d744d29
Olga <olga.kenobi@gmail.com>
2021-04-15 17:42:55 +0300

    Remove unnecessary code

--- Strassen/Strassen/main.cpp
+++ Strassen/Strassen/main.cpp
@@ -1,6 +1,5 @@
 //	C includes
 #include <stdlib.h>
-#include <time.h>
 
 //	Local includes
 #include "Matrix.h"
@@ -9,7 +8,6 @@ int main()
 {
 	const auto& randomize = []() 
 	{
-//		srand(time(NULL));
 		return rand() % 100;
 	};
 	Matrix<int> a = Matrix<int>::generate_matrix(2, 2, randomize);
